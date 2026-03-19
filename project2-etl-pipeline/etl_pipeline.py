c916a51441652e4ad27bc5c8f7488d79b313941d
Anil Pirla <137474429+anilpirla@users.noreply.github.com>
2026-03-19 12:16:32 +0530

    Update etl_pipeline.py

--- project2-etl-pipeline/etl_pipeline.py
+++ project2-etl-pipeline/etl_pipeline.py
@@ -1,1 +1,24 @@
 
+import pandas as pd
+import mysql.connector
+
+# Connect to AWS RDS
+conn = mysql.connector.connect(
+    host="YOUR_RDS_ENDPOINT",
+    user="admin",
+    password="YOUR_PASSWORD",
+    database="company_db"
+)
+
+# Extract
+users = pd.read_sql("SELECT * FROM users", conn)
+orders = pd.read_sql("SELECT * FROM orders", conn)
+
+# Transform
+merged = pd.merge(users, orders, on="user_id")
+merged['amount'] = merged['amount'] * 1.1  # Increase by 10%
+
+# Load (save CSV)
+merged.to_csv("data/final_data.csv", index=False)
+
+print("ETL completed successfully!")
