9f790d296440c7771b9d988a4c42c9782287d55d
Anil Pirla <137474429+anilpirla@users.noreply.github.com>
2026-03-19 12:05:38 +0530

    Update insert_data.sql

---
+++ project2-etl-pipeline/sql/insert_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (name, city) VALUES
+('Anil', 'Hyderabad'),
+('Ravi', 'Chennai'),
+('John', 'Bangalore');
+
+INSERT INTO orders (user_id, amount) VALUES
+(1, 1000),
+(2, 2000),
+(1, 1500),
+(3, 3000);
